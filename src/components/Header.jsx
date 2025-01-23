dffb30bdda3cb126d6eb3dd0ce4fa1c2a5865264
joelepore <giuseppejoelepore@gmail.com>
2025-01-23 15:38:52 +0100

    feat: implement dynamic header navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,16 @@
 import { NavLink } from "react-router-dom"
+import menuData from "../data/menuData"
 
 const Header = () => {
   return (
     <header>
       <nav className="h-14 bg-neutral-200">
         <ul className="h-full flex gap-6 items-center justify-center">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/posts">Post</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">Chi siamo</NavLink>
-          </li>
+          {menuData.map(item => (
+            <li key={item.id}>
+              <NavLink to={item.url}>{item.text}</NavLink>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
---
+++ src/data/menuData.js
@@ -0,0 +1,5 @@
+export default menuData = [
+  { id: 1, text: 'Home', url: '/' },
+  { id: 2, text: 'Post', url: '/posts' },
+  { id: 3, text: 'Chi siamo', url: '/about' },
+]
